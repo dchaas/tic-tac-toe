9b622217546553b70ca9164b687e7f8d067473d3
Daniel Haas <daniel.c.haas@gmail.com>
2022-07-02 14:03:00 -0400

    Setup the game board in html. Create game board divs and placeholders for player names and new game button. No functionality yet.

---
+++ tictactoe.js
@@ -0,0 +1,11 @@
+// create the gameboard with a module
+const Gameboard = (()=> {
+    let gameboard = ['x','x','o','o','x','o','x','o','o'];
+
+    const render = () => {
+
+    }
+
+
+    return  {gameboard};
+})();
