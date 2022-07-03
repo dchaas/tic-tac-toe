e24f8151f0565746450c7e75e8abe6eceb1321d6
Daniel Haas <daniel.c.haas@gmail.com>
2022-07-03 13:06:33 -0400

    Added ability to click on tile and fill in space. Accomplished with modules for the game state and gameboard, factory function for the players.

--- tictactoe.js
+++ tictactoe.js
@@ -1,6 +1,54 @@
+// create the player factory function
+let Player = (mark) => {
+    let _name = 'Player';
+    let marker = mark;
+
+    const setName = function(newName) {
+        _name = newName;
+    }
+
+    const getName = function() {
+        return _name;
+    }
+
+    return {marker, setName, getName}
+};
+
+// create the game state module
+let game = (()=>{
+    const player1 = Player('o');
+    const player2 = Player('x');
+    let _activePlayer = player1;
+    const turn = document.querySelector('#name');
+
+    const _render = function() {
+        turn.innerHTML=`${_activePlayer.getName()} (${_activePlayer.marker})`;
+    };
+
+    const changePlayer = ()=> {
+        _activePlayer = (_activePlayer===player1) ? player2 : player1;
+        _render();
+    };
+
+    const getActivePlayer = ()=> {
+        return _activePlayer;
+    };
+
+    _render();
+    return {getActivePlayer,changePlayer};
+})();
+
 // create the gameboard with a module
-const gameboard = (()=> {
-    let board = ['x','x','o','o','x','o','x','o','o'];
+const gameboard = ((gm)=> {
+    let board = ['','','','x','','','','',''];
+    const spaces = document.querySelectorAll('.space');
+
+    const _init = () => {
+        for (let i=0; i<spaces.length; i++) {
+            spaces[i].addEventListener('click',updateBoard)
+        }
+        _render();
+    }
 
     const _render = () => {
         board.forEach((square,i)=>{
@@ -10,11 +58,19 @@ const gameboard = (()=> {
         });
     }
 
-    const updateBoard = function() {
-
+    const updateBoard = function(event) {
+        // check to see if the box is already filled
+        console.log(event.target.id);
+        if (event.target.textContent === '') {
+            let index = event.target.id.replace('box','');
+            board[index-1] = gm.getActivePlayer().marker;
+            console.log(gm);
+            gm.changePlayer();
+            _render();
+        } 
     };
-
+    _init();
     return  {board,updateBoard,_render};
-})();
+})(game);
 
-gameboard._render();
+
